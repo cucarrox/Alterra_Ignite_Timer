180107d7fd3f4294f04159c2e58b1b60be53ef91
cucarrox <brunocucarrox@gmail.com>
2024-07-01 13:09:49 -0300

    Alterra

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,10 +14,17 @@ export function Home() {
         >
           <div className="flex items-center gap-4">
             <label className="text-2xl" htmlFor="task">Vou trabalhar em</label>
-            <input className="bg-transparent flex-1 h-10 border-0 border-b-2 border-b-primaryText font-bold text-[1.125rem] px-0 py-2 text-primaryText outline-none transition placeholder:text-secondary focus:shadow-none focus:border-blueLight" id="task" placeholder="Digite a sua tarefa" />
+            <input className={`${styled['task-input']} bg-transparent flex-1 h-10 border-0 border-b-2 border-b-primaryText font-bold text-[1.125rem] px-0 py-2 text-primaryText outline-none transition placeholder:text-secondary focus:shadow-none focus:border-blueLight`} id="task" placeholder="Digite a sua tarefa" list="task-suggestion" />
 
             <label className="text-2xl" htmlFor="minutesAmount">Durante</label>
-            <input className="bg-transparent w-16 h-10 border-0 border-b-2 border-b-primaryText font-bold text-[1.125rem] px-0 py-2 text-primaryText outline-none transition placeholder:text-secondary focus:shadow-none focus:border-blueLight" type="number" name="" id="minutesAmount" placeholder="00" />
+            <input className="bg-transparent w-16 h-10 border-0 border-b-2 border-b-primaryText font-bold text-[1.125rem] px-0 py-2 text-primaryText outline-none transition placeholder:text-secondary focus:shadow-none focus:border-blueLight" type="number" name="" id="minutesAmount" placeholder="00" step={5} min={5} max={60} />
+
+            <datalist id="task-suggestion">
+              <option value="Projeto Alterra" />
+              <option value="Estudar Programação" />
+              <option value="Estudar fisíca quântica" />
+              <option value="Não fazer nada" />
+            </datalist>
 
             <span className="text-2xl">minutos</span>
           </div>
@@ -32,7 +39,7 @@ export function Home() {
 
           <div className="w-full flex justify-center">
 
-            <Button disabled type="submit" className="w-2/5 text-2xl p-4">
+            <Button type="submit" className="w-2/5 text-2xl p-4">
               <PlayCircle size={40} />
               <span>Começar</span>
             </Button>
